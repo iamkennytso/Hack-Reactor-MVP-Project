2e8e7952bb886f8b1a4bcd0265416c478fcd2c34
Kenneth Tso <iamkennytso@gmail.com>
2017-09-16 15:09:02 -0400

    hard coded database credentails for deloyment?

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const cred = require('./cred')
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb')
 
-mongoose.connect(`mongodb://${cred}@ds135534.mlab.com:35534/hackreactormvp`, (err, win) => {
+mongoose.connect(`mongodb://testuser:testtest@ds135534.mlab.com:35534/hackreactormvp`, (err, win) => {
 	if(err){
 		console.error(`Error connecting: ${err}`)
 	} else {
